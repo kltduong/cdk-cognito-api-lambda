bb72a507bd74d6ec87198b75d70bc3956f96eee8
Duong Kieu <kltduong@gmail.com>
2020-09-17 06:03:34 +0700

    [f] Added authorizer for GET resource

--- lib/cognito-api-lambda-stack.ts
+++ lib/cognito-api-lambda-stack.ts
@@ -1,20 +1,17 @@
 import * as apigateway from '@aws-cdk/aws-apigateway';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
+import * as cognito from '@aws-cdk/aws-cognito';
 import * as path from 'path';
 
-
-const getLambdaPaths = function(lambda_name: String) {
-  const codePath = path.resolve(
-    __dirname,
-    `../src/lambda/${lambda_name}`
-  )
+const getLambdaPaths = function (lambda_name: String) {
+  const codePath = path.resolve(__dirname, `../src/lambda/${lambda_name}`);
   const envPath = path.resolve(
     __dirname,
     `../python_modules/lambda/${lambda_name}`
-  )
-  return { codePath, envPath }
-}
+  );
+  return { codePath, envPath };
+};
 
 export class CognitoApiLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -26,15 +23,34 @@ export class CognitoApiLambdaStack extends cdk.Stack {
       handler: 'main.handler',
       runtime: lambda.Runtime.PYTHON_3_8,
     });
-    
+
     const hloApi = new apigateway.LambdaRestApi(this, 'hloApi', {
-      restApiName: "HLO Service",
-      handler:hloLambda,
+      restApiName: 'HLO Service',
+      handler: hloLambda,
       proxy: false,
-    })
-    
+    });
+
+    const userPool = new cognito.UserPool(this, 'hloUserpool', {
+      signInAliases: {
+        email: true,
+      },
+    });
+
+    const authorizer = new apigateway.CfnAuthorizer(this, 'hloCfnauth', {
+      restApiId: hloApi.restApiId,
+      name: 'HelloAPIauthorizer',
+      type: 'COGNITO_USER_POOLS',
+      identitySource: 'method.request.header.Authorization',
+      providerArns: [userPool.userPoolArn],
+    });
+
     const hloResource = hloApi.root.addResource('hlo');
     const hloIntegration = new apigateway.LambdaIntegration(hloLambda);
-    hloResource.addMethod('GET', hloIntegration)
+    hloResource.addMethod('GET', hloIntegration, {
+      authorizationType: apigateway.AuthorizationType.COGNITO,
+      authorizer: {
+        authorizerId: authorizer.ref,
+      },
+    });
   }
 }
